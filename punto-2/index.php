4021d32bdf34206a12a4149d47bf544bf46a788e
SofiaOviedo24 <sofiagarcia4200@gmail.com>
2023-11-24 23:36:19 -0500

    punto 2 finalizado e imagenes del punto 1 y 2

---
+++ punto-2/index.php
@@ -0,0 +1,14 @@
+<?php
+$firstNumber = 0; 
+const SECOND_NUMBER = 5;
+
+$result =  SECOND_NUMBER + $firstNumber;
+
+echo $firstNumber . "+" . SECOND_NUMBER . "=" . $result . "\n";
+
+$firstNumber = 5; 
+
+$result =  SECOND_NUMBER + $firstNumber;
+
+echo $firstNumber . "+" . SECOND_NUMBER . "=" . $result . "\n";
+?>
